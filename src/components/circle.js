781b7327c93b61fec8ff17b536890195ec2b5fe5
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2022-12-28 15:34:23 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
+var Shape = require('./shape.js');
 // import using require
 
 // declare class
+class Circle extends Shape{
+  constructor(color){
+  super();
+  }
+calculateArea(){}
+}
 
-
-// export class using module.exports
+// export class using 
+module.exports=Circle;
